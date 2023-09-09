fd2cdee106c3aba79a33e5d82c0cac7378463e73
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-09-09 21:13:18 +0600

    code updated

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export async function GET(req, res) {
-  const { searchParams } = new URL(req.url);
-  let toEmail = searchParams.get("email");
-
+export async function POST(req, res) {
   // transporter
   let Transporter = nodemailer.createTransport({
     host: "mail.teamrabbil.com",
@@ -21,16 +18,16 @@ export async function GET(req, res) {
 
   // prepare email
   let myEmail = {
-    from: "Email Varification <info@teamrabbil.com>",
+    from: "Email Verification <info@teamrabbil.com>",
     to: toEmail,
-    subject: "New user Registration varification email",
+    subject: "New user Registration verification email",
     text: "Welcome to our website. You are a registered user from now. You can enjoy all of our services from us",
   };
 
   try {
     await Transporter.sendMail(myEmail);
     return NextResponse.json({
-      msg: "Email Sending Succesfull",
+      msg: "Email Sending Successful",
     });
   } catch (error) {
     return NextResponse.json({
--- src/app/emailvarify/page.js
+++ src/app/emailvarify/page.js
@@ -1,27 +1,36 @@
 "use client";
-import { useState } from "react";
-import Appnav from "@/components/Appnav";
 import { useRouter } from "next/navigation";
+import Appnav from "@/components/Appnav";
+import { useState, useEffect } from "react";
 
 export default function EmailVarify() {
   const router = useRouter();
 
-  // const [formData, setFormData] = useState({
-  //   varifyToken: "",
-  // });
-
   const handleBtn = async () => {
     try {
-      const res = await fetch("/api/email");
-      if (res.ok) {
-        router.replace("/dashboard");
+      if (router.query && "email" in router.query) {
+        const email = router.query.email;
+
+        const res = await fetch(`/api/email`);
+        if (res.ok) {
+          router.replace("/dashboard");
+        } else {
+          console.log(email);
+        }
       } else {
-        console.error("Email sending failed");
+        console.log(email);
       }
     } catch (error) {
       console.error("Error sending email:", error);
     }
   };
+
+  useEffect(() => {
+    if (router.query && "email" in router.query) {
+      const email = router.query.email;
+      setFormData({ varifyToken: email });
+    }
+  }, [router.query]);
 
   return (
     <>
@@ -29,7 +38,7 @@ export default function EmailVarify() {
       <div className="h-screen w-full flex items-center justify-center">
         <div className="w-[500px] bg-gray-100 shadow-md rounded p-6 ">
           <h1 className=" text-center text-xl font-bold mb-5">
-            Your are Registered
+            You are Registered
           </h1>
           <div className="login-form text-center">
             <button
